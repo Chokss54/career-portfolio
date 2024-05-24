f4c9bcc08048452881bfca2348efae6cd384d9bb
Chokss54 <joelyang54@gmail.com>
2024-05-24 15:31:43 +1000

    Updates

---
+++ precache-manifest.e0f39d2f1deb3deb01b57f6b57c9ce00.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509cb1a39c576d1bb054ac02c4a20dab",
+    "url": "/career-portfolio/index.html"
+  },
+  {
+    "revision": "aba7d37ee8ad35b78999",
+    "url": "/career-portfolio/static/css/main.a5aef045.chunk.css"
+  },
+  {
+    "revision": "27341bd746edb4b1ae44",
+    "url": "/career-portfolio/static/js/2.03e450d3.chunk.js"
+  },
+  {
+    "revision": "aba7d37ee8ad35b78999",
+    "url": "/career-portfolio/static/js/main.2af3264a.chunk.js"
+  },
+  {
+    "revision": "75accbd026d9a591b450",
+    "url": "/career-portfolio/static/js/runtime~main.b0568077.js"
+  },
+  {
+    "revision": "8ff7adab0e7b3180723e7d276a8a69a5",
+    "url": "/career-portfolio/static/media/Intake Functionality.8ff7adab.png"
+  },
+  {
+    "revision": "5765b95a507f663c0d3ce6066ab520cd",
+    "url": "/career-portfolio/static/media/Intake demo pic.5765b95a.png"
+  },
+  {
+    "revision": "350ec9ac0d4762ef92c9da88fe19bd7e",
+    "url": "/career-portfolio/static/media/TMT demo pic.350ec9ac.png"
+  },
+  {
+    "revision": "816353fbc0dbf62cb9d599ed9c7b1381",
+    "url": "/career-portfolio/static/media/TMT team.816353fb.jpg"
+  },
+  {
+    "revision": "2872818cfeb15d5e949f896d75bd985b",
+    "url": "/career-portfolio/static/media/graduatepic.2872818c.png"
+  },
+  {
+    "revision": "2fc42a669a7ddf706d285653cb208e3f",
+    "url": "/career-portfolio/static/media/wave green.2fc42a66.png"
+  }
+]);
